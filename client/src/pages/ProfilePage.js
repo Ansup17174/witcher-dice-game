8044b6f56f1780b2260bb6fee56bd191b93428a9
Ansup17174 <ansuported@op.pl>
2021-03-04 08:46:39 +0100

    user profile fix

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,23 +1,42 @@
-import {useContext} from 'react';
+import {useState, useContext, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import GlobalContext from '../GlobalContext';
 import {Container, Row} from '../components/containers';
 import Header from '../components/Header';
 
 const ProfilePage = () => {
     
+    const [userProfile, setUserProfile] = useState({
+        username: "",
+        email: {
+            address: ""
+        },
+        profile: {
+            matches_won: 0,
+            matches_lost: 0,
+            matches_played: 0
+        }
+    })
     const {userData} = useContext(GlobalContext);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!userData.id) history.push("/");
+        setUserProfile(userData);
+    }, [])
+
     const calculateWinRatio = number => {
         return Math.round(number * 100) + '%';
     };
     return (
         <Container>
             <Header>Profile</Header>
-            <Row><span>Username:</span><span>{userData.username}</span></Row>
-            <Row><span>E-mail:</span><span>{userData.email.address}</span></Row>
-            <Row><span>Matches won:</span><span>{userData.profile.matches_won}</span></Row>
-            <Row><span>Matches lost:</span><span>{userData.profile.matches_lost}</span></Row>
-            <Row><span>Matches played:</span><span>{userData.profile.matches_played}</span></Row>
-            <Row><span>Win ratio:</span><span>{calculateWinRatio(userData.profile.matches_won / userData.profile.matches_played)}</span></Row>
+            <Row><span>Username:</span><span>{userProfile.username}</span></Row>
+            <Row><span>E-mail:</span><span>{userProfile.email.address}</span></Row>
+            <Row><span>Matches won:</span><span>{userProfile.profile.matches_won}</span></Row>
+            <Row><span>Matches lost:</span><span>{userProfile.profile.matches_lost}</span></Row>
+            <Row><span>Matches played:</span><span>{userProfile.profile.matches_played}</span></Row>
+            <Row><span>Win ratio:</span><span>{calculateWinRatio(userProfile.profile.matches_won / userProfile.profile.matches_played)}</span></Row>
         </Container>
     );
 };

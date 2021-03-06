da6daaf114447ce3a9cd1497c628b27bda780995
Ansup17174 <ansuported@op.pl>
2021-03-06 13:00:36 +0100

    user tests

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -20,7 +20,7 @@ def db():
     yield TestingSessionLocal()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def user_register_dict() -> dict[str, str]:
     return {
         "username": "Janusz",
@@ -30,12 +30,17 @@ def user_register_dict() -> dict[str, str]:
     }
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def user_login_dict() -> dict[str, str]:
     return {
         "username": "Janusz",
         "password": "krowa123"
     }
+
+
+@pytest.fixture(scope="session")
+def token(user_login_dict) -> str:
+    return user_service.generate_access_token({'sub': user_login_dict['username']})
 
 
 def test_register_user(user_register_dict: dict[str, str]):
@@ -52,4 +57,25 @@ def test_cannot_login_without_confirmation(user_login_dict: dict[str, str]):
 
 def test_user_login(user_login_dict: dict[str, str], db: Session):
     user = user_service.get_user(db=db, username=user_login_dict['username'])
-    print(user)
+    user.email.is_confirmed = True
+    db.commit()
+    response = testclient.post("/auth/login", json=user_login_dict)
+    assert response.ok
+
+
+def test_user_change_password(user_login_dict: dict[str, str], token: str):
+    old_password = user_login_dict['password']
+    new_password1 = "aaaaaaaaaa"
+    new_password2 = new_password1
+    bad_new_password = "bbbbbbbbb"
+    data = {
+        "old_password": old_password,
+        "new_password1": new_password1,
+        "new_password2": bad_new_password
+    }
+    headers = {"Authorization": f"Bearer {token}"}
+    bad_response = testclient.post("/auth/change-password", json=data, headers=headers)
+    assert bad_response.status_code == 422
+    data.update(new_password2=new_password2)
+    response = testclient.post("/auth/change-password", json=data, headers=headers)
+    assert response.ok
